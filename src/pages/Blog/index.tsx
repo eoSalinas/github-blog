05d7e010b39b8f9f244a0effd33a8a2311e62b7f
Salinas <matheussalinas3@gmail.com>
2022-11-20 19:38:59 -0300

    Visual structure - PostCard

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,59 @@
 import { Profile } from './components/Profile'
 import { SearchForm } from './components/SearchForm'
+import { PostCard, PostContainer } from './style'
 
 export function Blog() {
   return (
     <>
       <Profile />
       <SearchForm />
+
+      <PostContainer>
+        <PostCard>
+          <span>
+            <h2>JavaScript data types and data structures</h2>
+            <p>Há 1 dia</p>
+          </span>
+          <p>
+            Programming languages all have built-in data structures, but these
+            often differ from one language to another. This article attempts to
+            list the built-in data structures available in...
+          </p>
+        </PostCard>
+        <PostCard>
+          <span>
+            <h2>JavaScript data types and data structures</h2>
+            <p>Há 1 dia</p>
+          </span>
+          <p>
+            Programming languages all have built-in data structures, but these
+            often differ from one language to another. This article attempts to
+            list the built-in data structures available in...
+          </p>
+        </PostCard>
+        <PostCard>
+          <span>
+            <h2>JavaScript data types and data structures</h2>
+            <p>Há 1 dia</p>
+          </span>
+          <p>
+            Programming languages all have built-in data structures, but these
+            often differ from one language to another. This article attempts to
+            list the built-in data structures available in
+          </p>
+        </PostCard>
+        <PostCard>
+          <span>
+            <h2>JavaScript data types and data structures</h2>
+            <p>Há 1 dia</p>
+          </span>
+          <p>
+            Programming languages all have built-in data structures, but these
+            often differ from one language to another. This article attempts to
+            list the built-in data structures available in
+          </p>
+        </PostCard>
+      </PostContainer>
     </>
   )
 }
--- src/pages/Blog/style.ts
+++ src/pages/Blog/style.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components'
 
-export const BlogContainer = styled.div``
+export const PostContainer = styled.main`
+  max-width: 864px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem;
+  margin-bottom: 234px;
+`
+
+export const PostCard = styled.div`
+  width: 26rem;
+  padding: 2rem;
+
+  background: ${(props) => props.theme['base-post']};
+  border-radius: 10px;
+
+  span {
+    display: flex;
+    justify-content: space-between;
+
+    h2 {
+      max-width: 17.6875rem;
+      margin-bottom: 1.25rem;
+
+      color: ${(props) => props.theme['base-title']};
+    }
+
+    p {
+      font-size: 0.875rem;
+      color: ${(props) => props.theme['base-span']};
+    }
+  }
+
+  p {
+    color: ${(props) => props.theme['base-text']};
+  }
+`
